ac95bed72c94bd3d147b4af97cc1db8c681fe360
Aidar Garikhanov <a1d4r@yandex.ru>
2020-04-18 18:20:18 +0300

    Add models for students, trainers and groups

--- web_app/sport_course/apps/classes/models.py
+++ web_app/sport_course/apps/classes/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    first_name = models.CharField(50)
+    last_name = models.CharField(50)
+    email = models.EmailField()
+    active = models.BooleanField("Is student's account active?")
+    health_group = models.ForeignKey("HealthGroup", on_delete=models.SET_NULL,
+                                     verbose_name="Health group of a student")
+
+
+class Trainer(models.Model):
+    first_name = models.CharField(50)
+    last_name = models.CharField(50)
+    email = models.EmailField()
+    active = models.BooleanField("Is trainer's account active?")
+
+
+class HealthGroup(models.Model):
+    name = models.CharField(20)
+
+
+class Group(models.Model):
+    name = models.CharField(10)
+    max_student_number = models.PositiveSmallIntegerField(default=20)
+    trainers = models.ManyToManyField("Trainer", verbose_name="Trainers of the group")
+    students = models.ManyToManyField("Student", verbose_name="Students enrolled in the group")
