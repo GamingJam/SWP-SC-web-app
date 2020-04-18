5e14c4d0413474ea9d924ae82e92a665041851fb
Aidar Garikhanov <a1d4r@yandex.ru>
2020-04-18 18:34:40 +0300

    Fix models

--- web_app/classes/models.py
+++ web_app/classes/models.py
@@ -2,27 +2,27 @@ from django.db import models
 
 
 class Student(models.Model):
-    first_name = models.CharField(50)
-    last_name = models.CharField(50)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     email = models.EmailField()
-    active = models.BooleanField("Is student's account active?")
-    health_group = models.ForeignKey("HealthGroup", on_delete=models.SET_NULL,
+    active = models.BooleanField("Is student's account active?", default=True)
+    health_group = models.ForeignKey("HealthGroup", on_delete=models.SET_NULL, null=True,
                                      verbose_name="Health group of a student")
 
 
 class Trainer(models.Model):
-    first_name = models.CharField(50)
-    last_name = models.CharField(50)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     email = models.EmailField()
-    active = models.BooleanField("Is trainer's account active?")
+    active = models.BooleanField("Is trainer's account active?", default=True)
 
 
 class HealthGroup(models.Model):
-    name = models.CharField(20)
+    name = models.CharField(max_length=20)
 
 
 class Group(models.Model):
-    name = models.CharField(10)
+    name = models.CharField(max_length=10)
     max_student_number = models.PositiveSmallIntegerField(default=20)
     trainers = models.ManyToManyField("Trainer", verbose_name="Trainers of the group")
     students = models.ManyToManyField("Student", verbose_name="Students enrolled in the group")
