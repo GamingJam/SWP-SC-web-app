b0831e1c53e5e6a05dcd827fa5a22c02029e2b88
Aidar Garikhanov <a1d4r@yandex.ru>
2020-04-20 14:17:01 +0300

    Make primitive dashboard page

--- web_app/classes/views.py
+++ web_app/classes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 
 def index(request):
@@ -7,7 +8,14 @@ def index(request):
 
 
 def register(request):
-    context = {}
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'form': form}
     return render(request, 'classes/register.html', context)
 
 
