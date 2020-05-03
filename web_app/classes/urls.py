da646ef887a413f30d47b1e7dc88997c18b03938
Aidar Garikhanov <a1d4r@yandex.ru>
2020-05-03 03:10:21 +0300

    Add authorization

--- web_app/classes/urls.py
+++ web_app/classes/urls.py
@@ -3,9 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('login/', views.login, name='login'),
-    path('register/', views.register, name='register'),
+    path('', views.index, name='home'),
     path('classes/', views.classes_schedule, name='classes_schedule'),
     path('group/<int:pk>', views.sport_group, name='sport_group'),
     path('groups/', views.groups, name='sport_groups'),
--- web_app/classes/views.py
+++ web_app/classes/views.py
@@ -5,24 +5,7 @@ from .models import *
 
 
 def index(request):
-    return render(request, 'classes/dashboard.html')
-
-
-def register(request):
-    form = UserCreationForm()
-
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-
-    context = {'form': form}
-    return render(request, 'classes/register.html', context)
-
-
-def login(request):
-    context = {}
-    return render(request, 'classes/login.html', context)
+    return render(request, 'classes/home.html')
 
 
 def classes_schedule(request):
