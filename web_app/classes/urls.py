d348dba16896537aea0e330842aea72c8558c18b
Aidar Garikhanov <a1d4r@yandex.ru>
2020-05-11 03:58:43 +0300

    Add navigation bar, reformat HTML files

--- web_app/classes/urls.py
+++ web_app/classes/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('profile/', views.profile, name='profile'),
     path('classes/', views.classes_schedule, name='classes_schedule'),
     path('group/<int:pk>', views.sport_group, name='sport_group'),
     path('groups/', views.groups, name='sport_groups'),
--- web_app/classes/views.py
+++ web_app/classes/views.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 
 
 def home(request):
-    return render(request, 'classes/home.html')
+    if request.user.is_authenticated:
+        return redirect('profile')
+    else:
+        return redirect('login')
 
 
 def classes_schedule(request):
@@ -18,6 +21,11 @@ def sport_group(request, pk):
 
     context = {'group': group}
     return render(request, 'classes/group.html', context)
+
+
+@login_required
+def profile(request):
+    return render(request, 'classes/profile.html')
 
 
 def groups(request):
